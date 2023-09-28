d69369ee087ced6dfe5930bca305cfa367e28ced
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-09-28 21:11:20 +0700

    Update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 -- mathlib brings std, aesop, doc-gen4, Qq as dependencies
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "e70f8800f70f89b102ad7469128ace674213e1e5"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "b86770a38f6ee8d4a7f64cebcf388fb8b8cef89b"
 
 package FierceNerds.Util where
   -- add package configuration options here
