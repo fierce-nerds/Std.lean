740086ae398d2f238aba4d5e660dc34770fc7489
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-10-02 11:57:19 +0700

    List

--- FierceNerdsUtil/List/Fun/Sep.lean
+++ FierceNerdsUtil/List/Fun/Sep.lean
@@ -1,6 +1,4 @@
-namespace FierceNerdsUtil.Data.List
-
-namespace String
+namespace List
 
 def foldlSep (separator : String) (l : List String) : String :=
   l.foldl (fun r s => r ++ separator ++ s) ""
